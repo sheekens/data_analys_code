9d029b8a279d2bb295b000301a483346a06af87b
unknown <aleksander.cherviakov@gmail.com>
2023-07-26 13:20:01 +0300

    fixed to draw in boundbox

--- romb.py
+++ romb.py
@@ -3,28 +3,29 @@ import cv2
 import numpy as np
 from varname.helpers import debug
 
-def draw_romb_on_img (img_path, line_color, line_thickness):
+def draw_romb (img_path, top_left_xy, bot_right_xy, line_color, line_thickness):
     img = cv2.imread(img_path)
     img2draw = img.copy()
-    img_height, img_width, img_channels = img2draw.shape
-    x_step = int(img_width / 2)
-    y_step = int(img_height / 2)
-    current_x = 0
-    current_y = 0
+    debug(img2draw.shape)
+    bbox_height = abs(bot_right_xy[1] - top_left_xy[1])
+    bbox_width = abs(bot_right_xy[0] - top_left_xy[0])
+    
+    x_step = int(bbox_width / 2)
+    y_step = int(bbox_height / 2)
+    current_x = top_left_xy[0]
+    current_y = top_left_xy[1]
 
+    img2draw = cv2.rectangle(img2draw, top_left_xy, bot_right_xy, colors_dict['blue'], line_thickness)
     for i in range(2):
         previous_y = current_y
         current_y += y_step
-        current_x = 0
-        if current_y > img_height:
-            current_y = img_height
+        current_x = top_left_xy[0]
         for j in range(2):
             previous_x = current_x
             current_x += x_step
-            if current_x > img_width:
-                current_x = img_width
             if i%2 != 0 and j%2==0 or i%2==0 and j%2!=0:
                 img2draw = cv2.line(img2draw, pt1=(previous_x, previous_y), pt2=(current_x, current_y), color=line_color, thickness=line_thickness)
+                debug(previous_x, previous_y, current_x, current_y)
             else:
                 img2draw = cv2.line(img2draw, pt1=(previous_x, current_y), pt2=(current_x, previous_y), color=line_color, thickness=line_thickness)
     return img2draw
@@ -40,7 +41,13 @@ colors_dict = {
     'pink':(203, 192, 255)
     }
 
-chess_jeezus = draw_romb_on_img('jeezus.jpg', colors_dict['pink'], 6)
-# print(colors_dict['green'])
+pt1_top_left_xy = (30, 20)
+pt2_bot_right_xy = (480, 40)
+chess_jeezus = draw_romb(
+    img_path='uz.jpg',
+    top_left_xy=pt1_top_left_xy,
+    bot_right_xy=pt2_bot_right_xy,
+    line_color=colors_dict['pink'],
+    line_thickness=1)
 cv2.imshow('jeezus', chess_jeezus)
 cv2.waitKey(-1)
